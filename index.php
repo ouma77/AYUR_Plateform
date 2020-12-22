21132f6e959f5727182f5cb830ef0347d44d5f61
oumayma essemrhouni <42737226+ouma77@users.noreply.github.com>
2020-12-22 14:09:46 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,320 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+  <title>AYUR : travel to impact</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="style1.css">
+  <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+</head>
+
+
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#myPage">AYUR</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#about">Acceuil</a></li>
+        <li><a href="#services">A propos</a></li>
+        <li><a href="#portfolio">Achat Solidaire</a></li>
+        <li><a href="#pricing">Packs</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </div>
+  </div> 
+</nav>
+
+<div class="jumbotron jump1 text-center">
+            <div id="titr">
+                <h1>TRAVEL TO IMPACT</h1>
+                <p>AYUR est un service de voyage social, qui vous permet de visitez les plus belles<br>destinations au Maroc, tout en aidant les enfants de la régions <br> à découvrir le monde à travers VOUS!</p>
+                <button type="button" class="btn btn1">Devenez Ayurist!</button>
+                <button type="button" class="btn btn2" data-toggle="modal" data-target="#partners">Devenez partenaires!</button>
+
+                <center><button type="button" class="btne btne1">Devenez Ayurist!</button>
+                <button type="button" class="btne btne2" data-toggle="modal" data-target="#partners">Devenez partenaires!</button></center>
+                
+            </div>
+</div>
+
+<!-- Story -->
+<div class="part">
+         <h2>Découvrez notre histoire unique!</h2>
+         <iframe class="vid" src="https://www.youtube.com/embed/4Its0qjjjv8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class="jumbotron jump2">
+                 <h1 class="display-4">Story!</h1>
+                 <p class="lead">Amusez vous à voir les photos de nos anciens voyages!</p>
+                 <hr class="my-4">
+                 <p>On se réunit tous ici pour laisser notre trace dans ce monde, <br>
+                    et inspirer les enfants qui ont besoin de notre soutien!</p>
+                    <a  href="https://www.instagram.com/ayur_travel_to_impact/" class="themeBtn4">Follow us!</a>
+                 
+            </div>
+</div>
+
+
+
+
+<div class="container">
+  <h2 style="text-align:center">Découvrez nos prochains Packs</h2>  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="img/event2.png" alt="Los Angeles" style="width:100%;">
+      </div>
+
+      <div class="item">
+        <img src="img/event1.png" alt="Chicago" style="width:100%;">
+      </div>
+    
+      <div class="item">
+        <img src="img/event3.jpg" alt="New york" style="width:100%;">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+<!-- </div> -->
+
+<!-- Container (Pricing Section) -->
+<div id="pricing" class="container-fluid">
+  <div class="text-center">
+    <h2>Nos offres</h2>
+    <h4>Because we are different!</h4>
+  </div>
+  <div class="row slideanim">
+    <div class="col-sm-4 col-xs-12">
+      <div class="panel pan1 panel-default text-center">
+        <div class="panel-heading panel-heading1">
+          <h1>Réservation par personne</h1>
+        </div>
+        <div class="panel-body">
+        <p><strong>Date :</strong> Déjà Programmé</p>
+        <hr>
+          <p><strong>Hébergement :</strong> Inclus</p>
+          <hr>
+          <p><strong>Nourriture :</strong> Inclus</p>
+          <hr>
+          <p><strong>Transport :</strong> Inclus</p>
+          <hr>
+          <p><strong>Accompagnateur :</strong> Inclus</p>
+          <hr>
+          <p><strong>Activité Social :</strong> Inclus</p>
+
+        </div>
+        <div class="panel-footer">
+          <button class="btn btn-lg bt1">Voir plus</button>
+        </div>
+      </div>      
+    </div>     
+    <div class="col-sm-4 col-xs-12">
+      <div class="panel pan2 panel-default text-center">
+        <div class="panel-heading panel-heading2">
+        <h1>Réservation par groupe</h1>
+        </div>
+        <div class="panel-body">
+        <p><strong>Date :</strong> Votre choix</p>
+        <hr>
+          <p><strong>Hébergement :</strong> Inclus</p>
+          <hr>
+          <p><strong>Nourriture :</strong> Inclus</p>
+          <hr>
+          <p><strong>Transport :</strong> Inclus</p>
+          <hr>
+          <p><strong>Accompagnateur :</strong> Inclus</p>
+          <hr>
+          <p><strong>Activité Social :</strong> Inclus</p>
+        </div>
+        <div class="panel-footer">
+          <button class="btn btn-lg bt2">Voir plus</button>
+        </div>
+      </div>      
+    </div>       
+    <div class="col-sm-4 col-xs-12">
+      <div class="panel pan3 panel-default text-center">
+        <div class="panel-heading panel-heading3">
+          <h1>Réservation par personne</h1>
+        </div>
+        <div class="panel-body">
+          <p><strong>Date :</strong> Votre choix</p>
+          <hr>
+          <p><strong>Hébergement :</strong> Inclus</p>
+          <hr>
+          <p><strong>Nourriture :</strong> Inclus</p>
+          <hr>
+          <p><strong>Transport :</strong> Non Inclus</p>
+          <hr>
+          <p><strong>Accompagnateur :</strong> Non Inclus</p>
+          <hr>
+          <p><strong>Activité Social :</strong> Inclus</p>
+        </div>
+        <div class="panel-footer">
+          <button class="btn btn-lg bt3">Voir plus</button>
+        </div>
+      </div>      
+    </div>    
+  </div>
+</div>
+
+<!-- Container (Contact Section) -->
+<div id="contact" class="container-fluid bg-grey">
+  <h2 class="text-center">Abonner-vous!</h2>
+  <form>
+    <div class="input-group">
+      <input type="email" class="form-control" size="50" placeholder="Email Address" required>
+      <div class="input-group-btn">
+        <button type="button" class="btn btn-primary">Subscribe</button>
+      </div>
+    </div>
+  </form>
+</div>
+
+<!-- Image of location/map -->
+
+<footer class="container-fluid text-center">
+<section id="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+					<ul class="list-unstyled list-inline social text-center">
+						<li class="list-inline-item"><a href="#"><i class="fa fa-facebook"></i></a></li>
+						
+						<li class="list-inline-item"><a href="#"><i class="fa fa-instagram"></i></a></li>
+						
+						<li class="list-inline-item"><a href="#"><i class="fa fa-envelope"></i></a></li>
+					</ul>
+				</div>
+		</div>
+	</section>
+
+  <p>&copy All right Reversed. AYUR : travel to impact</p>
+</footer>
+
+
+<!-- Modal -->
+<div id="partners" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h5 class="modal-title" id="exampleModalScrollableTitle" style="color:teal; text-align:center; font-size:22px">Partenaires : Demande de collaboration</h5>
+      </div>
+      <div class="modal-body">
+      <strong><p style="text-align: center">Vous voulez qu'on collabore ensemble ?<br>
+           Laissez nous vos coordonnées ! Nous vous contacterons dans les plus brefs des délais.</p></strong> 
+           <hr>
+            <form action="insertPart.php" method="post">
+               <div>
+                  <label>Nom :<br><input type="text" name="nom" placeholder="Nom"></label>
+                  <label>Prénom :<br><input type="text" name="Prenom" placeholder="Prénom"></label>
+                  <label>Téléphone :<br><input type="tel" name="tel" placeholder="Numéro de téléphone"></label>
+               </div>
+               <div>
+                  <label>Email : <br><input type="email" name="email" placeholder="Pseudo@gmail.com" style="width: 319%"></label>
+               </div>
+               <div>
+                  <label>Organisme : <br><input type="text" name="email" placeholder="Association/Coopérative/Auberge..." style="width: 319%"></label>
+               </div>
+               <div>
+                  <label>Ajouter un dossier :<br><input type="file" name="file" style="width: 178%"></label>
+               </div>
+               <div>
+                  <label>Message de plus! <br><textarea name="message" rows="10" cols="72">
+                  </textarea></label>
+               </div>
+               <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+          <button type="submit" class="btn btn-danger">Envoyer</button>
+        </div>
+        </form>
+
+  </div>
+</div>
+  <!-- boostrap JS -->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script>
+$(document).ready(function(){
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+  
+  $(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+          $(this).addClass("slide");
+        }
+    });
+  });
+})
+</script>
+
+</body>
+</html>
